1d906b95ec39aeb9edd23bf7a659e56cb6b5a75e
Fabio Chen <59851467+fabioyebchen@users.noreply.github.com>
2023-08-19 14:39:19 -0400

    Mobile Responsive Register/Login

--- src/page-turners/user/register-screen.js
+++ src/page-turners/user/register-screen.js
@@ -3,65 +3,100 @@ import Nav from "../../nav";
 import { useNavigate } from "react-router";
 import { useDispatch } from "react-redux";
 import { registerThunk } from "../services/auth-thunks";
-import "./user.css"; 
+import "./user.css";
 import { NavLink } from "react-router-dom";
 
 function RegisterScreen() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const handleRegister = async () => {
-        try {
-            await dispatch(registerThunk({ username, password, firstName, lastName }));
-            navigate("/tuiter/profile");
-        } catch (e) {
-            alert(e);
-        }
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleRegister = async () => {
+    try {
+      await dispatch(
+        registerThunk({ username, password, firstName, lastName })
+      );
+      navigate("/tuiter/profile");
+    } catch (e) {
+      alert(e);
     }
-    return (
-        <div className="register-container">
-            <Nav/>
-        <div className="register-form">
-            <h1 className="register-title">Register</h1>
-            <form>
-            <div className="form-group mt-2">
-                <label className="form-label" htmlFor="username">Username</label>
-                <input className="form-control" type="text" value={username} id="username"
+  };
+  return (
+    <div className="register-container">
+      <Nav />
+      <div className="register-form">
+        <h1 className="register-title">Register</h1>
+        <form>
+          <div className="form-group mt-2">
+            <label className="form-label" htmlFor="username">
+              Username
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              value={username}
+              id="username"
               placeholder="Choose a username"
-                       onChange={(event) => setUsername(event.target.value)}/>
-            </div>
-            <div className="form-group mt-2">
-                <label className="form-label" htmlFor="password">Password</label>
-                <input className="form-control" type="password" value={password} id="password"
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </div>
+          <div className="form-group mt-2">
+            <label className="form-label" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="form-control"
+              type="password"
+              value={password}
+              id="password"
               placeholder="Create a password"
-                       onChange={(event) => setPassword(event.target.value)}/>
-            </div>
-            <div className="form-group mt-2">
-                <label className="form-label" htmlFor="firstName">First Name</label>
-                <input className="form-control" type="text" value={firstName} id="firstName"
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          <div className="form-group mt-2">
+            <label className="form-label" htmlFor="firstName">
+              First Name
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              value={firstName}
+              id="firstName"
               placeholder="Enter your first name"
-                       onChange={(event) => setFirstName(event.target.value)}/>
-            </div>
-            <div className="form-group mt-2">
-                <label className="form-label" htmlFor="lastName">Last Name</label>
-                <input className="form-control" type="text" value={lastName} id="lastName"
+              onChange={(event) => setFirstName(event.target.value)}
+            />
+          </div>
+          <div className="form-group mt-2">
+            <label className="form-label" htmlFor="lastName">
+              Last Name
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              value={lastName}
+              id="lastName"
               placeholder="Enter your last name"
-                       onChange={(event) => setLastName(event.target.value)}/>
-            </div>
-            <button className="btn btn-primary mt-3" onClick={handleRegister}>
+              onChange={(event) => setLastName(event.target.value)}
+            />
+          </div>
+          <button className="btn btn-primary mt-3" onClick={handleRegister}>
             Register
-            </button>
-            <div className="mt-2">   
-            <NavLink className="nav-link" activeClassName="active" exact to="/login">
-                Already have an account?    
+          </button>
+          <div className="mt-2">
+            <NavLink
+              className="nav-link"
+              activeClassName="active"
+              exact
+              to="/login"
+            >
+              Already have an account?
             </NavLink>
-          </div>   
-          </form>
+          </div>
+        </form>
       </div>
     </div>
   );
-};
+}
 export default RegisterScreen;

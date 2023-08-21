05b414a9591bce4e3a3b5558b7c805fc04c32dfd
Fabio Chen <59851467+fabioyebchen@users.noreply.github.com>
2023-08-20 21:55:46 -0400

    added heroku path

--- src/page-turners/services/auth-service.js
+++ src/page-turners/services/auth-service.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
-const USERS_URL = `${SERVER_API_URL}/users`;
+// const USERS_URL = `${SERVER_API_URL}/users`;
+const USERS_URL = `https://page-turner-node-18efed809441.herokuapp.com/api/users`;
+
 const api = axios.create({ withCredentials: true });
 
 
